4208272976395b23caf62d5324af1965a3e3533f
RohitBhojak <rohitbhojak99@gmail.com>
2024-06-16 14:40:11 +0530

    Add change size option and coloring

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-let container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("grid");
-        row.append(square);
+function createGrid(size) {
+    container.textContent = "";
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement("div");
+            square.classList.add("grid");
+            row.append(square);
+        }
+        container.append(row);
     }
-    container.append(row);
 }
+
+function color() {
+    container.addEventListener("mouseover", (event) => {
+        if (event.target.classList.contains("grid")) {
+            event.target.style.backgroundColor = "black";
+        }
+    });
+}
+
+function getSize() {
+    let button = document.querySelector("#size");
+    button.addEventListener("click", () => {
+        let size = prompt("Enter size (Smaller than 100) : ");
+        size = parseInt(size);
+        if (size > 0 && size < 100) {
+            createGrid(size);
+        } else {
+            alert("Invalid size!");
+        }
+    })
+}
+
+const container = document.querySelector(".container");
+createGrid(16);
+getSize();
+color();
